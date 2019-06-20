488c462db5feb013ddd340461c23e2c9426245ea
SeleneHdz01 <hdz_f@hotmail.com>
2019-06-20 09:49:01 -0500

    Se hace disponible componente de orden superior con *compose* para generar función de anidamiento y comunicar componentes entre si

--- gdl002-framework-social-network/src/components/Firebase/index.js
+++ gdl002-framework-social-network/src/components/Firebase/index.js
@@ -1,3 +1,4 @@
+//Se hace disponible el componente de orden superior "withFirebase"
 import FirebaseContext, { withFirebase } from "./context";
 import Firebase from "./firebase";
 
--- gdl002-framework-social-network/src/components/SignUp/signUp.js
+++ gdl002-framework-social-network/src/components/SignUp/signUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
-// import { compose } from "recompose";
+import { compose } from "recompose";
 
 import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
@@ -19,6 +19,7 @@ const INITIAL_STATE = {
   passwordTwo: "",
   error: null
 };
+
 class SignUpFormBase extends Component {
   constructor(props) {
     super(props);
@@ -32,7 +33,7 @@ class SignUpFormBase extends Component {
       .doCreateUserWhitEmailAndPassword(email, passwordOne)
       .then(authUser => {
         this.setState({ ...INITIAL_STATE });
-        // this.props.history.push(ROUTES.HOME);
+        this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
         this.setState({ error });
@@ -96,7 +97,13 @@ const SignUpLink = () => (
     ¿No tienes una cuenta? <Link to={ROUTES.SIGN_UP}>Regístrate</Link>
   </p>
 );
+//Función de anidamiento (paracomponentes de orden superior) para comunicarse entre sí.
+//"Compose" aplica a los componentesde orden superios de derecha a izquierda
+const SignUpForm = compose(
+  withRouter,
+  withFirebase
+)(SignUpFormBase);
 
-const SignUpForm = withFirebase(SignUpFormBase);
+// const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 export default SignUpPage;
 export { SignUpForm, SignUpLink };

0c50404d664e662a9618226d56f46185d9011dcf
SeleneHdz01 <hdz_f@hotmail.com>
2019-06-25 17:21:10 -0500

    Iniciando base de datos en tiempo real con Firebase

--- gdl002-framework-social-network/src/components/Firebase/firebase.js
+++ gdl002-framework-social-network/src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -15,6 +16,7 @@ class Firebase {
     app.initializeApp(firebaseConfig);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
   //Auth API (Interfaz de Autenticación para componentes React que se conectaran a la API de FIrebase).
   doCreateUserWithEmailAndPassword = (email, password) =>
